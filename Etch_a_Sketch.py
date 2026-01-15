201833a9a4d9679ab9b3920409834430ab93e880
AKCAS7 <138361450+AKCAS7@users.noreply.github.com>
2026-01-15 16:36:50 +0530

    Update Etch_a_Sketch.py

--- Etch_a_Sketch.py
+++ Etch_a_Sketch.py
@@ -3,9 +3,31 @@ from turtle import Turtle, Screen
 t = Turtle()
 screen = Screen()
 
-
+''' Learning listening 
 def move():
     t.forward(10)
 screen.listen()
 screen.onkey(key = "space", fun = move)
 screen.exitonclick()
+'''
+
+#Building the game
+
+def move_forwards():
+    t.forward(10)
+
+def move_backwards():
+    t.backward(10)
+
+def clockwise():
+    t.right(5)
+
+def anti_clockwise():
+    t.left(5)
+
+
+screen.listen()
+screen.onkey( key = "w", fun = move_forwards)
+screen.onkey( key = "s", fun = move_backwards)
+screen.onkey( key = "a", fun = anti_clockwise)
+screen.onkey( key = "d", fun = clockwise)
